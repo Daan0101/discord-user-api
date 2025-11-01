a0536cc1c4c711c24fa15dea1cf97ec7a7f225e5
daan0101 <173400@student.talland.nl>
2025-11-01 22:16:32 +0100

    feat: return type user

--- src/controller/discord.go
+++ src/controller/discord.go
@@ -1,9 +1,23 @@
 package controller
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+
+	"github.com/daan0101/discordapi/src/types"
+	"github.com/daan0101/discordapi/src/client"
+)
 
 func GetDiscordUser(c *fiber.Ctx) error {
-	return c.JSON(fiber.Map{
-		"ID": c.Params("id"),
-	})
+	id := c.Params("id")
+
+	var user types.User
+	client := client.NewClient()
+	err := client.Get("/users/"+id, &user)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": err.Error(),
+		})
+	}
+
+	return c.JSON(user)
 }
