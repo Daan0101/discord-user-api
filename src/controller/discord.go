34abee1a3272ae11c1ec4658e293921f6ab063f7
daan0101 <173400@student.talland.nl>
2025-11-01 21:34:53 +0100

    feat: json res

--- src/controller/discord.go
+++ src/controller/discord.go
@@ -3,5 +3,7 @@ package controller
 import "github.com/gofiber/fiber/v2"
 
 func GetDiscordUser(c *fiber.Ctx) error {
-	return c.SendString("Discord User Endpoint")
+	return c.JSON(fiber.Map{
+		"ID": c.Params("id"),
+	})
 }
