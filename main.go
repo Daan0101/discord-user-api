d2df237061d371a8e691971d30262ea7c8cb8960
daan0101 <173400@student.talland.nl>
2025-11-01 20:38:57 +0100

    feat: gofiber

--- main.go
+++ main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
+
+	log.Fatal(app.Listen(":3000"))
 }
